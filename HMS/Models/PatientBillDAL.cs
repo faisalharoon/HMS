f1d57856836b236671e4b9d9b7848ec96540590b
SadiaRashid92 <sadiarashid92@hotmail.com>
2019-06-11 22:07:58 +0500

    PatientBill Module Added

---
+++ HMS/Models/PatientBillDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Models
+{
+    public class PatientBillDAL
+    {
+        HMS_DBEntity db = new HMS_DBEntity();
+        public List<tblPatientBill> GetAllRecords()
+        {
+            return db.tblPatientBills.ToList();
+
+        }
+
+
+    }
+}
