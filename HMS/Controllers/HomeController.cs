b8a107958b6871199f1443531bf8696b920e7bcb
Gull Noor <gullnoor13@hotmail.com>
2019-08-04 23:59:53 +0500

    c

--- HMS/Controllers/HomeController.cs
+++ HMS/Controllers/HomeController.cs
@@ -13,12 +13,13 @@ namespace HMS.Controllers
         private HMS_DBEntities db = new HMS_DBEntities();
         public ActionResult Index()
         {
-            DashboardViewModel model = new DashboardViewModel();
-            model.DashboardsModel = db.Database.SqlQuery<DashboardModel>("[Sp_Dashboard]").ToList();
-            model.ActivePatientsList = db.Database.SqlQuery<ActivePatientList>("[Sp_ActivePatientsList]").ToList();
-            db.Database.ExecuteSqlCommand("PatientAdmission");//Trigger it
-            model.PatientsCount = db.Database.SqlQuery<PatientCount>("[Patient_Count]").ToList();
-            model.RoomsOccupied = db.Database.SqlQuery<RoomOccupied>("[Sp_RoomsOccupied]").ToList();
+            DashboardViewModel model = new DashboardViewModel
+            {
+                DashboardsModel = db.Database.SqlQuery<DashboardModel>("[Sp_Dashboard]").ToList(),
+                ActivePatientsList = db.Database.SqlQuery<ActivePatientList>("[Sp_ActivePatientsList]").ToList(),
+                PatientsCount = db.Database.SqlQuery<PatientCount>("[Patient_Count]").ToList(),
+                RoomsOccupied = db.Database.SqlQuery<RoomOccupied>("[Sp_RoomsOccupied]").ToList()
+            };
             return View(model);
         }
 
@@ -32,8 +33,10 @@ namespace HMS.Controllers
             //var result = lst.Where(r => Convert.ToDateTime(r.AdmissionDate).Month == month);
 
             var list = db.tblPatientAdmissions.GroupBy(r => Convert.ToDateTime(r.AdmissionDate).Month == month && r.IsActive == true).Count();
-            PatientCount obj = new PatientCount();
-            obj.PatientsAdmittedCurrentMonth = list;
+            PatientCount obj = new PatientCount
+            {
+                PatientsAdmittedCurrentMonth = list
+            };
 
             //.Select(u => new PatientCount
             //     {
@@ -63,6 +66,23 @@ namespace HMS.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Menu()
+        {
+            int ID = 0;
+            List<GetMenu_Result> Menu = db.GetMenu().Where(x => x.Active_role == true && x.IsActive == true && x.is_active == true && x.show_in_menu == true).ToList();
+              HttpCookie cookie = Request.Cookies["AdminCookies"];
+            if (cookie != null)
+            {
+                ID = Convert.ToInt32(cookie.Values["ID"]);
+                Menu = Menu.Where(x => x.ID == ID).ToList();
+            }
+            else
+            {
+                Menu = null;
+            }
+          
+            return PartialView("Menu", Menu);
         }
     }
 }
