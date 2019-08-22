44fa3686c2dd44c46a77914affc039c771bf62a3
SadiaRashid92 <sadiarashid92@hotmail.com>
2019-08-22 11:46:14 +0500

    Save Changes

--- HMS/Models/tblPatientBill.cs
+++ HMS/Models/tblPatientBill.cs
@@ -28,6 +28,7 @@ namespace HMS.Models
         public Nullable<System.DateTime> CreatedAt { get; set; }
         public string CreatedBy { get; set; }
         public string Description { get; set; }
+        public Nullable<bool> is_active { get; set; }
     
         public virtual tblPatientAppointment tblPatientAppointment { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
